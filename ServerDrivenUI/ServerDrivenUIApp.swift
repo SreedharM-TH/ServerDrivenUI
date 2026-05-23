ac8960b5e9e8a623b67762fefb00f3f8c49d55fc
SreedharM-TH <sreedhar.mallu@thinkhat.ai>
2026-05-23 13:23:19 +0530

    Initial Commit

---
+++ ServerDrivenUI/ServerDrivenUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ServerDrivenUIApp.swift
+//  ServerDrivenUI
+//
+//  Created by sreedhar on 23/05/26.
+//
+
+import SwiftUI
+
+@main
+struct ServerDrivenUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
